fb19391acc239f5639ed82bed7823dbc11a521d9
Reyki <reykigarcia@gmail.com>
2022-12-20 21:39:47 -0600

    new html files

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def homePageView(request):
-    return HttpResponse("Hello, World!")
+class HomePageView(TemplateView):  # capital HomePageView since it is class
+    template_name = "home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+
+# def homePageView(request):     # lowercase homePageView since it is a function
+# return HttpResponse("Hello, World!")
 
 
 # Create your views here.
