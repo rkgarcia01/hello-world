62e4b8e70845f615a0b302105957b246ddf4bb2d
Reyki <reykigarcia@gmail.com>
2023-01-21 15:37:25 -0600

    added comments to urls.py/pages:

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-# from .views import homePageView
+# from .views import homePageViewd
+# By referring to the view.py file as .view we are telling Django to look within the current directory for a view.py file and import the CBV, FBV, or GCBV
 from .views import HomePageView, AboutPageView
 
 # we use .as_view() when useing a Class-Based View in 'views.py'
